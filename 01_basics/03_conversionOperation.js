18792e7174d2db588a2d07725d8f0872b54e5a4b
SR Lal <sukhramlal@gmail.com>
2023-11-19 13:31:40 -0500

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "lla"
+
+//console.log(score)
+//console.log(typeof score);
+
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+
+// "56" => 56
+// "56abc" => NaN
+// "true" => 1; false => 0
+
+let isLoggedIn = "you"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false 
+// "you" => true
+
+let someNumber = 35
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,4 @@
 console.log("Hello Sukhram Lal");
+
+
+/* You can't, however,  /* nest comments *\/  SyntaxError */
