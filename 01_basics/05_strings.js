8dcebcd7ecd234508668485111bed4321f95f5aa
SR Lal <sukhramlal@gmail.com>
2023-11-27 17:27:20 -0500

    String

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,39 @@
+const lname = "lal"
+const repoCount = 3
+
+//console.log(lname + repoCount + " Value");
+
+//console.log(`My last name is ${lname.toUpperCase()} and my repo count is ${repoCount}`)
+
+const gameName = new String('Slal-js-com')
+
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(4));
+//console.log(gameName.indexOf('l'));
+
+
+//const newString = gameName.substring(0, 5)
+//console.log(newString);
+
+// const anotherString = gameName.slice(0, 3)
+// console.log(anotherString);
+
+// const newStringOne = ' Lal S  ';
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+// console.log(newStringOne.trimStart());
+// console.log(newStringOne.trimEnd());
+
+// const url = "https://goo.com/Slal%20Lal";
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('lal1'));
+
+// console.log(gameName.split('-'));
+
+// console.log(gameName.small());
+
