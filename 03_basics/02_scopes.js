ffa650969d0dacc350d33bb540011babebbfde0a
SR Lal <sukhramlal@gmail.com>
2023-12-08 20:25:58 -0500

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,16 @@
+let a = 10
+const b = 20
+
+
+
+if (true) {
+    let a = 400
+    var c = 30
+    console.log("inner a is :" , a);
+//    console.log(b);
+}
+
+c = 300
+
+console.log("global a is :" , a);
+console.log(c);
