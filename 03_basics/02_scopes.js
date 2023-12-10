6d375c582557467d6dfa88b72a4d9b3eacf40bde
SR Lal <sukhramlal@gmail.com>
2023-12-09 22:47:54 -0500

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,11 +6,55 @@ const b = 20
 if (true) {
     let a = 400
     var c = 30
-    console.log("inner a is :" , a);
+//    console.log("inner a is :" , a);
 //    console.log(b);
 }
 
 c = 300
 
-console.log("global a is :" , a);
-console.log(c);
+// console.log("global a is :" , a);
+// console.log(c);
+
+
+function one(){
+    const username = "SLAL"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    //console.log(website);
+
+     //two()
+
+}
+
+//one()
+
+// if (true) {
+//     const username = "SLAL"
+//     if (username === "SLAL") {
+//         const website = " youtube"
+//         //console.log(username + website);
+//     }
+//     //console.log(website);
+// }
+
+// //console.log(username);
+
+
+// ++++++++++++++++++ interesting ++++++++++++++++++
+
+
+
+// console.log(addone(5))
+// function addone(num){
+//     return num + 1
+// }
+
+
+
+// addTwo(5)
+// const addTwo = function(num){
+//     return num + 2
+// }
