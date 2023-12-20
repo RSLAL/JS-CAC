e2705517fa021da7b7ca79c5e07cd2452458c004
SR Lal <sukhramlal@gmail.com>
2023-12-19 23:11:24 -0500

    loop

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -12,6 +12,55 @@ for (const num of arr) {
 
 const greetings = "Hello world!";
 for (const greet of greetings) {
- //   console.log(`Each char is ${greet}`);
+ //console.log(`Each char is ${greet}`);
 }
+
+// array.forEach(element => {
+    
+// });
+
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
+
+// for (const iterator of object) {
+    
+// }
+
+// Maps , it is for unique values
+
+const map = new Map();
+
+map.set('IN', 'India')
+map.set('USA', 'United States of America')
+map.set('Fr', 'France')
+map.set('IN', 'India')
+
+//console.log(map);
+
+for (const key of map) {
+ //   console.log(key);
+}
+
+for (const [key, value] of map) {
+    //console.log(key, ':-', value);
+}
+
+// const myObject = {
+//     'game1': 'NFS',
+//     'game2': 'Spiderman'
+// }
+
+const myObject = {
+    game1: 'NFS',
+    game2: 'Spiderman'
+}
+
+//myObject is not iterable
+// for (const [key, value] of myObject) {
+//     console.log(key, ':-', value);
+// }
 
